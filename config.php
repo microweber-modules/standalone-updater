a060e64522834d333aa0b4d4aa7a977980995bd7
Bozhidar Slaveykov <bobi@microweber.com>
2021-04-21 16:43:14 +0300

    update

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$config = array();
+$config['name'] = "Standalone Updater";
+$config['author'] = "Microweber";
+$config['no_cache'] = false;
+$config['ui'] = true;
+$config['categories'] = "other";
+$config['position'] = 39;
+$config['version'] = 1;
