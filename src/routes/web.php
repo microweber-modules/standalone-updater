1a97d8c9c40ab4f826e615deb29ffea036b5ffd0
Bozhidar Slaveykov <bobi@microweber.com>
2021-04-22 11:00:06 +0300

    update

--- src/routes/web.php
+++ src/routes/web.php
@@ -9,23 +9,26 @@
 
 api_expose_admin('standalone-update-now', function () {
 
-    $updateCacheFolderName = 'standalone-update';
+    $updateCacheFolderName = 'standalone-update/' . rand(222,444) . time(). '/' ;
     $updateCacheDir = base_path() . DS . $updateCacheFolderName;
+
     delete_recursive($updateCacheDir);
     mkdir_recursive($updateCacheDir);
 
-    $randomFileUpdateName = rand(222,444) . time(). '-mw-update.php';
+    $randomFolderUpdateName = rand(222,444) . time(). '-mw-update.php';
 
-    $redirectLink = site_url() . $updateCacheFolderName .'/'. $randomFileUpdateName;
+    $redirectLink = site_url() . $updateCacheFolderName;
 
     // copy(  dirname(__DIR__) . '/mw-black-logo.png', $updateCacheDir . DS . 'mw-black-logo.png');
-
    //  copy(  dirname(__DIR__) . '/Microweber-logo-reveal.mp4', $updateCacheDir . DS . 'Microweber-logo-reveal.mp4');
 
-    $sourceUpdater = file_get_contents(dirname(__DIR__) .'/StandaloneUpdateExecutor.source');
+    $sourceUpdater = file_get_contents(dirname(__DIR__) .'/standalone-installation-setup/actions.source');
+    $saveActions = file_put_contents($updateCacheDir . DS . 'actions.php', $sourceUpdater);
 
-    $save = file_put_contents($updateCacheDir . DS . $randomFileUpdateName, $sourceUpdater);
-    if ($save) {
+    $sourceUpdater = file_get_contents(dirname(__DIR__) .'/standalone-installation-setup/index.source');
+    $saveIndex = file_put_contents($updateCacheDir . DS . 'index.php', $sourceUpdater);
+
+    if ($saveActions && $saveIndex) {
         return ['success'=>true,'redirect_to'=>$redirectLink];
     }
 
