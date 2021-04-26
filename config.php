890f46e16c333f49b6a17f0865c74f0c79d69332
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2021-04-26 16:05:23 +0300

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ $config['ui_admin'] = true;
 $config['is_system'] = true;
 $config['categories'] = "other";
 $config['position'] = 1;
-$config['version'] = 1.1;
+$config['version'] = 1.2;
 
 $config['settings']['service_provider'] = [
     \MicroweberPackages\StandaloneUpdater\StandaloneUpdaterServiceProvider::class
