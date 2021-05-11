a0e0b59eacfe15c536add8982481bfaf27ccc855
Bozhidar Slaveykov <bobi@microweber.com>
2021-05-11 18:09:48 +0300

    update

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ $config['ui_admin'] = true;
 $config['is_system'] = true;
 $config['categories'] = "other";
 $config['position'] = 1;
-$config['version'] = 1.3;
+$config['version'] = 1.4;
 
 $config['settings']['service_provider'] = [
     \MicroweberPackages\StandaloneUpdater\StandaloneUpdaterServiceProvider::class
--- functions.php
+++ functions.php
@@ -2,8 +2,10 @@
 autoload_add_namespace(__DIR__ . '/src/', 'MicroweberPackages\\StandaloneUpdater\\');
 
 function latest_version() {
+
     $updateApi = 'https://update-dev.microweberapi.com/?api_function=get_download_link&get_last_version=1';
-    $version = file_get_contents($updateApi);
+    $version = app()->url_manager->download($updateApi);
+
     $version = json_decode($version, true);
     return $version;
 }
