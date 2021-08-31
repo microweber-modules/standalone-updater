b63488d3838e97aa130c3ab0e3cd5098c0332384
Nikolay <rachkuw@gmail.com>
2021-08-31 14:41:48 +0300

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ $config['ui_admin'] = true;
 $config['is_system'] = true;
 $config['categories'] = "other";
 $config['position'] = 1;
-$config['version'] = 2.2;
+$config['version'] = 2.3;
 
 $config['settings']['service_provider'] = [
     \MicroweberPackages\StandaloneUpdater\StandaloneUpdaterServiceProvider::class
