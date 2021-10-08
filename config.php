309719bcab0ba6d88c5d62df89c2bf773dcea90b
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-10-08 12:04:01 +0300

    update

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 $config = array();
 $config['name'] = "Standalone Updater";
-$config['author'] = "Microweber";
+$config['author'] = "bobi@microweber.com";
 $config['no_cache'] = false;
 $config['ui'] = false;
 $config['ui_admin'] = true;
 $config['is_system'] = true;
 $config['categories'] = "other";
 $config['position'] = 1;
-$config['version'] = 2.3;
+$config['version'] = 2.4;
 
 $config['settings']['service_provider'] = [
     \MicroweberPackages\StandaloneUpdater\StandaloneUpdaterServiceProvider::class
