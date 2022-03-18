a36e73e20992249b06dfa1b853fce3fedac4508f
Bozhidar Slaveykov <bobi@microweber.com>
2022-03-18 14:19:22 +0200

    Update about.blade.php

--- src/Resources/views/about.blade.php
+++ src/Resources/views/about.blade.php
@@ -1,1 +1,37 @@
-{!! $about !!}
+<style>
+    .about-markdown h1 {
+        font-size:16px;
+    }
+    .about-markdown p {
+        font-size:13px;
+    }
+    .about-markdown ul {
+        list-style-position: inside;
+    }
+    .about-new-version-block {
+        width:100%;
+        padding-left:40px;
+        padding-top:100px;
+        padding-bottom:100px;
+        background:#4592ff;
+        color:#fff;
+        margin-bottom: 20px;
+    }
+    .about-new-version-block h1 {
+        font-size:38px;
+    }
+    .about-new-version-block h3 {
+        font-size:20px;
+    }
+</style>
+<div class="container p-5">
+
+    <div class="about-new-version-block">
+        <h1>Microweber {{MW_VERSION}}</h1>
+        <h3>Drag and drop website builder.</h3>
+    </div>
+
+    <div class="about-markdown">
+        {!! $about !!}
+    </div>
+</div>
