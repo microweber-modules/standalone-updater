481c20c868bad34926b4fb38bf3fc8cc087d3085
Peter Ivanov <peter@microweber.com>
2022-07-11 19:13:14 +0300

    update

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ $config['ui_admin'] = true;
 $config['is_system'] = true;
 $config['categories'] = "other";
 $config['position'] = 1;
-$config['version'] = '3.6';
+$config['version'] = '3.7';
 
 $config['settings']['service_provider'] = [
     \MicroweberPackages\Modules\StandaloneUpdater\StandaloneUpdaterServiceProvider::class
