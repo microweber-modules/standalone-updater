285ca45f9a19ccb9afefa45fb9550a008e166aa1
Nikolay <rachkuw@gmail.com>
2024-03-13 15:22:21 +0200

    update

--- dashboard_notice.php
+++ dashboard_notice.php
@@ -14,15 +14,15 @@
     }
 </script>
 
-<div class="js-standalone-updater-dashboard-notice card border-primary mb-3">
+<div class="js-standalone-updater-dashboard-notice card border-primary mb-3 p-3">
     <div class="card-body">
-        <h4 class="card-title">New version is available!</h4>
-        <p class="card-text">Current version (<?php echo MW_VERSION; ?>)</p>
-        <p class="card-text">There is a new version (<?php echo $params['new-version']; ?>) available, do you want to update now?</p>
+        <h2 class="card-title font-weight-bold"><?php _e('New version is available'); ?>!</h2>
+        <p class="card-text"><?php _e('Current version'); ?> (<span class="font-weight-bold"><?php echo MW_VERSION; ?></span>)</p>
+        <p class="card-text"><?php _e('There is a new version'); ?> (<span class="font-weight-bold"><?php echo $params['new-version']; ?></span>) <?php _e('available, do you want to update now?') ?></p>
 
         <br />
 
-        <a href="#" onclick="standaloneUpdaterDashboardNoticeDissmiss()" class="btn btn-outline-primary">Later</a>
-        <a href="<?php echo module_admin_url('standalone-updater'); ?>" class="btn btn-outline-primary">Update now</a>
+        <a href="#" onclick="standaloneUpdaterDashboardNoticeDissmiss()" class="btn btn-link justify-content-center mw-admin-action-links mw-adm-liveedit-tabs me-3"><?php _e('Later') ?></a>
+        <a href="<?php echo module_admin_url('standalone-updater'); ?>" class="btn btn-link justify-content-center mw-admin-action-links mw-adm-liveedit-tabs"><?php _e('Update now') ?></a>
     </div>
 </div>
